9d24147dfb344c4bbf4cefa5eaed185d74a177ce
belal_soliman <belalsoliman8@gmail.com>
2024-09-29 11:08:23 +0300

    finish splash screen and navigate to  home

--- lib/features/splash/presentaion/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentaion/views/widgets/splash_view_body.dart
@@ -1,8 +1,22 @@
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/features/home/presentaion/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:bookly/consts.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    super.initState();
+    navigateToHomeWithAnimathion();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,5 +30,12 @@ class SplashViewBody extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void navigateToHomeWithAnimathion() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(() => const HomeView(),
+          transition: Transition.fadeIn, duration: kTransitionDuration);
+    });
   }
 }

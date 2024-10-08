8be524d2d6fe4d1565f61c3b77e4602ee6e3d43e
belal_soliman <belalsoliman8@gmail.com>
2024-10-08 07:07:51 +0300

    finish use cases

--- lib/features/home/domain/use_case/fetch_pop_books.dart
+++ lib/features/home/domain/use_case/fetch_pop_books.dart
@@ -1,19 +1,16 @@
 import 'package:bookly/core/errors/failure.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:bookly/features/home/domain/repos/home_repo.dart';
+import 'package:bookly/features/home/domain/use_case/fetceh_fetear_books_usecase.dart';
 import 'package:dartz/dartz.dart';
 
-class FetcehFetearBooksUsecase extends UseCase<List<BookEntity>> {
+class FetchPopBooksUseCae extends UseCase<List<BookEntity>> {
   final HomeRepo homeRepo;
 
-  FetcehFetearBooksUsecase({required this.homeRepo});
+  FetchPopBooksUseCae({required this.homeRepo});
 
   @override
   Future<Either<Failure, List<BookEntity>>> call() async {
-    return await homeRepo.fetchFeaturedBooks();
+    return await homeRepo.fetchNewestBooks();
   }
-}
-
-abstract class UseCase<T> {
-  Future<Either<Failure, T>> call();
 }

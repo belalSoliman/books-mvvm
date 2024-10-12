7d777b0dc89f4cbaf42050d492a954e2f952300c
belal_soliman <belalsoliman8@gmail.com>
2024-10-12 22:43:38 +0300

    create api functions

--- lib/core/utils/api_sevice.dart
+++ lib/core/utils/api_sevice.dart
@@ -2,9 +2,10 @@ import 'package:dio/dio.dart';
 
 class ApiService {
   final Dio _dio;
-  final String baseUrl = 'https://fakestoreapi.com';
+  final String baseUrl = 'https://www.googleapis.com/books/v1/';
 
   ApiService(this._dio);
+
   Future<Map<String, dynamic>> get({required String endpoint}) async {
     var response = await _dio.get('$baseUrl$endpoint');
     return response.data;
---
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -0,0 +1,36 @@
+import 'package:bookly/core/utils/api_sevice.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/domain/entities/book_entity.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewsetBooks();
+}
+
+class HomeRemoteDataSourceImplemntaion extends HomeRemoteDataSource {
+  final ApiService apiService;
+  HomeRemoteDataSourceImplemntaion({required this.apiService});
+  @override
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var data = await apiService.get(
+        endpoint: 'volumes?filtering=free-ebooks&q=programming');
+    List<BookEntity> books = getBookList(data);
+    return books;
+  }
+
+  @override
+  Future<List<BookEntity>> fetchNewsetBooks() async {
+    var data = await apiService.get(
+        endpoint: 'volumes?filtering=free-ebooks&sorting=newest&q=programming');
+    List<BookEntity> books = getBookList(data);
+    return books;
+  }
+
+  List<BookEntity> getBookList(Map<String, dynamic> data) {
+    List<BookEntity> books = [];
+    for (var item in data['items']) {
+      books.add(BookModel.fromJson(item));
+    }
+    return books;
+  }
+}

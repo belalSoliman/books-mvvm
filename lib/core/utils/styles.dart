45ce7add6bb465c1d9aebfb5d931ea69795fb0d6
belal_soliman <belalsoliman8@gmail.com>
2024-10-12 16:09:47 +0300

    enhance ui

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 abstract class Styles {
   static const titleMedduim = TextStyle(
-      fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xff292B38));
+      fontSize: 18, fontWeight: FontWeight.w500, color: Color(0xff292B38));
 
   static const titileLarge = TextStyle(
       fontSize: 24, fontWeight: FontWeight.bold, color: Color(0xff292B38));
